70ab30496ad4170ca986bbc411ec44175f19b8d5
Zsolt Fejes <zsolt.fejes@icloud.com>
2021-06-20 16:54:01 +0100

    Added blacklist and clear cache

--- SucuriAPI.go
+++ SucuriAPI.go
@@ -9,6 +9,9 @@ type Sucuri struct {
 	ApiSecret string
 }
 
+// TODO Show Settings
+// TODO Audit trail
+
 // UpdateSetting generates a SucuriRequest that will overwrite the specified setting and value
 func (s Sucuri) UpdateSetting(setting string, value string) SucuriRequest {
 	request := SucuriRequest{
@@ -21,6 +24,17 @@ func (s Sucuri) UpdateSetting(setting string, value string) SucuriRequest {
 	return request
 }
 
+// UpdateSetting generates a SucuriRequest that will clear the site cache
+func (s Sucuri) ClearCache() SucuriRequest {
+	request := SucuriRequest{
+		prefix: "Clearing Cache",
+		sucuri: s,
+		params: url.Values{},
+	}
+	request.params.Add("a", "clear_cache")
+	return request
+}
+
 // WhitelistIP generates a SucuriRequest that adds the specified IP address to the whitelisted IPs list.
 // If remove is set to true the specified IP address will be removed from the whitelisted IP addresses
 func (s Sucuri) WhitelistIP(ip string, remove bool) SucuriRequest {
@@ -29,6 +43,25 @@ func (s Sucuri) WhitelistIP(ip string, remove bool) SucuriRequest {
 	if remove {
 		action = "delete_allowlist_ip"
 		prefix = "Removing whitelisted IP"
+	}
+	request := SucuriRequest{
+		prefix: prefix + ip + "; ",
+		sucuri: s,
+		params: url.Values{},
+	}
+	request.params.Add("a", action)
+	request.params.Add("ip", ip)
+	return request
+}
+
+// WhitelistIP generates a SucuriRequest that adds the specified IP address to the whitelisted IPs list.
+// If remove is set to true the specified IP address will be removed from the whitelisted IP addresses
+func (s Sucuri) BlacklistIP(ip string, remove bool) SucuriRequest {
+	action := "blacklist_ip"
+	prefix := "Blacklisting IP"
+	if remove {
+		action = "delete_blacklist_ip"
+		prefix = "Removing blacklisted IP"
 	}
 	request := SucuriRequest{
 		prefix: prefix + ip + "; ",
